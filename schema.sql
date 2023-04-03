217238f5325a121e7213220cfead7ecabeeca465
Javier <javidx@gmail.com>
2023-04-03 20:16:22 +0300

    db and table created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,19 @@
-/* Database schema to keep the structure of entire database. */
+-- Creating database
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+createdb vet_clinic
+
+psql vet_clinic
+
+create table animals (
+  id int,
+  name varchar(100),
+  date_of_birth date,
+  escape_attempts int,
+  neutered boolean,
+  weight_kg decimal(6,4),
+  primary key(id));
+
+alter table animals alter column id set not null;
+
+alter table animals alter column id add generated always as identity;
+
