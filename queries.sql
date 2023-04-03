18270fc433775add6a1244222338644f49c66448
Javier <javidx@gmail.com>
2023-04-03 21:18:35 +0300

    Added first two queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,2 @@
-/*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where name like '%mon';
+select * from animals where date_of_birth between '2016-01-01' and '2019-12-31';
