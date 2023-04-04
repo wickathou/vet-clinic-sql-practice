ac56c6102a682acd752b18ca8b02a341db5f8ebe
Javier <javidx@gmail.com>
2023-04-04 15:06:09 +0300

    Added aggregator queries

--- queries.sql
+++ queries.sql
@@ -35,3 +35,35 @@ select * from animals;
 update animals set weight_kg = weight_kg*(-1) where weight_kg < 0;
 select * from animals;
 commit;
+
+-- Questions:
+-- How many animals are there?
+-- Answer: 10
+-- Query:
+select count(*) from animals;
+
+-- How many animals have never tried to escape?
+-- Answer: 2
+-- Query:
+select count(*) from animals where escape_attempts = 0;
+
+-- What is the average weight of animals?
+-- Answer: 15.55
+-- Query:
+select avg(weight_kg) from animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+-- Answer: 7 true, 3 false
+-- Query:
+select neutered,count(*) from animals group by neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+-- Answer: Pokemon (min:11, max:17), Digimon (min:5.7, max:45)
+-- Query:
+select species,min(weight_kg),max(weight_kg) from animals group by species;
+
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+-- Answer: Pokemon(3), Digimon(Null)
+-- Query:
+select species,avg(escape_attempts) from animals where date_part('year',date_of_birth) between 1990 and 2000 group by species;
