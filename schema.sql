e4d3793534092e489d0ee149227405fb05c8c17f
Javier <javidx@gmail.com>
2023-04-05 17:21:38 +0300

    Added new species and owners tables Added entries

--- schema.sql
+++ schema.sql
@@ -21,3 +21,35 @@ alter table animals alter column id add generated always as identity;
 -- Day 2
 
 alter table animals add column species varchar(100) ;
+
+-- Day 3
+
+create table owners(
+id serial primary key,
+fullname varchar(100),
+age int);
+
+select * from owners;
+
+create table species(
+id serial primary key,
+name varchar(100));
+
+select * from species;
+
+alter table animals drop column species;
+alter table animals add column species_id int, add column owner_id int;
+
+select * from animals;
+
+alter table animals                                                  
+add constraint species_fk
+foreign key (species_id)
+references species(id)
+on delete cascade;
+
+alter table animals                                                 
+add constraint owners_fk
+foreign key (owner_id)
+references owners(id)
+on delete cascade;
